5720153f3fc9fd14d81372fef3af5314b772cccd
jon <jonerra23@aim.com>
2015-09-24 18:34:16 +0000

    Add questions to admin panel

--- core/models.py
+++ core/models.py
@@ -7,6 +7,7 @@ class Question(models.Model):
   description = models.TextField(null=True, blank=True)
   created_at = models.DateTimeField(auto_now_add=True)
   user = models.ForeignKey(User)
+  
 
   def __unicode__(self):
     return self.title
