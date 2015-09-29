ebc244ec8ec3b62a68f5d594f7db696463ce52f3
jon <jonerra23@aim.com>
2015-09-29 18:22:39 +0000

    Add question update view

--- core/views.py
+++ core/views.py
@@ -1,26 +1,31 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView, CreateView, ListView, DetailView
+from django.views.generic import TemplateView, CreateView, ListView, DetailView, UpdateView
 from django.core.urlresolvers import reverse_lazy
 from .models import *
 
-# Create your views here.
+# Create your views here
 class Home(TemplateView):
     template_name = "home.html"
-  
+
 class QuestionCreateView(CreateView):
     model = Question
     template_name = "question/question_form.html"
     fields = ['title', 'description']
     success_url = reverse_lazy('question_list')
-    
+
     def form_valid(self, form):
       form.instance.user = self.request.user
       return super(QuestionCreateView, self).form_valid(form)
-    
+
 class QuestionListView(ListView):
     model = Question
     template_name = "question/question_list.html"
-    
+
 class QuestionDetailView(DetailView):
     model = Question
     template_name = "question/question_detail.html"
+
+class QuestionUpdateView(UpdateView):
+    model = Question
+    template_name = "question/question_form.html"
+    fields = ['title', 'description']
